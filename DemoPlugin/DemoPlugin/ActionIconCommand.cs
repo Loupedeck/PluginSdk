f0016b4eac9755975100bafa67aef5bb477bf979
rpodolianko <rpodolianko@logitech.com>
2025-06-10 10:12:28 +0300

    Add ict file example

---
+++ DemoPlugin/DemoPlugin/ActionIconCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Loupedeck.DemoPlugin
+{
+    public class ActionIconCommand : PluginDynamicCommand
+    {
+        public ActionIconCommand()
+            : base(displayName: "Action Icon", description: "Action Icon description", groupName: "Action Icons")
+        {
+        }
+    }
+}
